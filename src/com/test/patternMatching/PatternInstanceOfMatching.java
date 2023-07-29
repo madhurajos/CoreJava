c835b182e145aa90c451947a7e1a48fd79ae96db
madhurajos17 <madhurajos17@gmail.com>
2023-07-29 14:47:23 -0500

    changes in patternmatching

--- src/com/test/patternMatching/PatternInstanceOfMatching.java
+++ src/com/test/patternMatching/PatternInstanceOfMatching.java
@@ -18,16 +18,14 @@ public class PatternInstanceOfMatching {
 
 	@Override
 	public boolean equals(Object obj) {
-		/*if (this == obj)
+		if (this == obj)
 			return true;
 		if (obj == null)
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
 		PatternInstanceOfMatching other = (PatternInstanceOfMatching) obj;
-		return Objects.equals(str, other.str);*/
-		
-		return obj instanceof String s && str == s.str)
+		return Objects.equals(str, other.str);
 	}
 	
 	
